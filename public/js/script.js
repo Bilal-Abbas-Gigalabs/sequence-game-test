a8b9d0e55b5b3a3d502be19d81df0aa9e3d02540
Fazi-Fiaz <fiaz.yes@gmail.com>
2023-01-09 13:24:32 +0500

    JS Code Completed Sequence with double Combination || Resolved double click issue

--- public/js/script.js
+++ public/js/script.js
@@ -1,31 +1,31 @@
 // Single Deck Cards
-var cards = ["AS", "AH", "AD", "AC", "S2", "H2", "D2", "C2", "S3", "H3", "D3", "C3",
-    "S4", "H4", "D4", "C4", "S5", "H5", "D5", "C5", "S6", "H6", "D6", "C6",
-    "S7", "H7", "D7", "C7", "S8", "H8", "D8", "C8", "S9", "H9", "D9", "C9", "S10", "H10", "D10", "C10",
-    "JS", "JH", "JD", "JC", "QS", "QH", "QD", "QC", "KS", "KH", "KD", "KC"];
-
-// var cards = ["AS", "AH", "AD", "AC", "JS", "JH", "JD", "JC", "QS", "QH", "QD", "QC", "KS", "C3"];
-
-// var boardDeck = ["AS", "AH", "AD", "AC", "S2", "H2", "D2", "C2", "S3", "H3", "D3", "C3",
+// var cards = ["AS", "AH", "AD", "AC", "S2", "H2", "D2", "C2", "S3", "H3", "D3", "C3",
 //     "S4", "H4", "D4", "C4", "S5", "H5", "D5", "C5", "S6", "H6", "D6", "C6",
 //     "S7", "H7", "D7", "C7", "S8", "H8", "D8", "C8", "S9", "H9", "D9", "C9", "S10", "H10", "D10", "C10",
-//     "QS", "QH", "QD", "QC", "KS", "KH", "KD", "KC"];
+//     "JS", "JH", "JD", "JC", "QS", "QH", "QD", "QC", "KS", "KH", "KD", "KC"];
 
-var boardDeck = ["AC", "KC", "QC", "C10", "C9", "C8", "C7", "C6",
-            "AD", "S7", "S8", "S9","S10", "QS", "KS", "AS", "C5", "S2",
-            "KD", "S6", "C10", "C9", "C8", "C7","C6", "D2", "C4", "S3",
-            "QD", "S5", "QC", "H8", "H7", "H6", "C5", "D3", "C3", "S4",
-            "D10", "S4","KC", "H9", "H2", "H5", "C4", "D4", "C2", "S5",
-            "D9", "S3", "AC", "H10","H3", "H4", "C3", "D5", "AH", "S6",
-            "D8", "S2", "AD", "QH", "KH", "AH","C2", "D6", "KH", "S7",
-            "D7", "H2", "KD", "QD", "D10", "D9", "D8", "D7", "QH", "S8",
-            "D6", "H3","H4", "H5", "H6", "H7", "H8", "H9", "H10", "S9",
-            "D5", "D4", "D3", "D2", "AS", "KS", "QS", "S10"];
+var cards = ["AS", "AH", "AD", "AC", "JS", "JH", "JD", "JC", "QS", "QH", "QD", "QC", "KS", "C3"];
+
+var boardDeck = ["AS", "AH", "AD", "AC", "S2", "H2", "D2", "C2", "S3", "H3", "D3", "C3",
+    "S4", "H4", "D4", "C4", "S5", "H5", "D5", "C5", "S6", "H6", "D6", "C6",
+    "S7", "H7", "D7", "C7", "S8", "H8", "D8", "C8", "S9", "H9", "D9", "C9", "S10", "H10", "D10", "C10",
+    "QS", "QH", "QD", "QC", "KS", "KH", "KD", "KC"];
+
+// var boardDeck = ["AC", "KC", "QC", "C10", "C9", "C8", "C7", "C6",
+//             "AD", "S7", "S8", "S9","S10", "QS", "KS", "AS", "C5", "S2",
+//             "KD", "S6", "C10", "C9", "C8", "C7","C6", "D2", "C4", "S3",
+//             "QD", "S5", "QC", "H8", "H7", "H6", "C5", "D3", "C3", "S4",
+//             "D10", "S4","KC", "H9", "H2", "H5", "C4", "D4", "C2", "S5",
+//             "D9", "S3", "AC", "H10","H3", "H4", "C3", "D5", "AH", "S6",
+//             "D8", "S2", "AD", "QH", "KH", "AH","C2", "D6", "KH", "S7",
+//             "D7", "H2", "KD", "QD", "D10", "D9", "D8", "D7", "QH", "S8",
+//             "D6", "H3","H4", "H5", "H6", "H7", "H8", "H9", "H10", "S9",
+//             "D5", "D4", "D3", "D2", "AS", "KS", "QS", "S10"];
 
 // Double Decks
 let doubleDeck = cards.concat(cards);
-let boardDoubleDeck = boardDeck;
-// let boardDoubleDeck = boardDeck.concat(boardDeck);
+// let boardDoubleDeck = boardDeck;
+let boardDoubleDeck = boardDeck.concat(boardDeck);
 
 // BoardCards
 
@@ -901,10 +901,10 @@ clickHandwithBoard = () => {
         handcardClick[i].addEventListener('click', handcardClickFunction);
     }
 }
-
+let cardid = '';
 handcardClickFunction = function () {
-    console.log("Hand Cards Click")
-    let id = this.getAttribute("id");
+    cardid = this.getAttribute("id");
+    id = cardid;
     let deadcard = this.classList.contains("dead");
     if (deadcard) {
         DeadpushCard(id);
@@ -935,8 +935,9 @@ handcardClickFunction = function () {
 }
 boadCardClick = function () {
     let cardinfo = this;
-    var id = this.getAttribute('card');
-    var enableCards = document.querySelectorAll('#board-cards li[card="'+id+'"]')
+    let id = cardid;
+    let enableCards = document.querySelectorAll('#board-cards .enable');
+    let enableCardsRemove = document.querySelectorAll('#board-cards li');
     enableCards.forEach(enables => {
         enables.classList.remove('enable');
     });
